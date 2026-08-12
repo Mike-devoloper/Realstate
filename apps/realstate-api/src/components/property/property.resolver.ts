12b1458c870fdfcb0a60f5d58e50f936465fa0fc
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2026-08-13 00:46:39 +0900

    feat: updatePropertyByAdmin graphQl Api

--- apps/realstate-api/src/components/property/property.resolver.ts
+++ apps/realstate-api/src/components/property/property.resolver.ts
@@ -75,10 +75,21 @@ export class PropertyResolver {
     @Roles(MemberType.ADMIN)
     @UseGuards(RolesGuard)
     @Query(() => Properties)
-    public async getAllPRopertiesByAdmin(
+    public async getAllPropertiesByAdmin(
         @Args('input') input: AllPropertiesInquiry,
         @AuthMember('_id') memberId: ObjectId ):Promise<Properties> {
         console.log("Query: getAllPropertiesByAdmin");
         return await this.propertyService.getAllPropertiesByAdmin(input);
+    }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => Property)
+    public async updatePropertyByAdmin(
+        @Args("input") input: PropertyUpdate,
+        @AuthMember('_id') memberId: ObjectId): Promise<Property> {
+        console.log("Mutation updatePropertyByAdmin executed");
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.propertyService.updatePropertyByAdmin(input);
     }
 }
