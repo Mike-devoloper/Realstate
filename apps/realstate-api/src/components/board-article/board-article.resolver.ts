0e2cff2d1d7b350df157e74fc02117db98509df7
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2026-08-18 22:22:57 +0900

    feat: develop admin related boardArticle GraphQl Api

--- apps/realstate-api/src/components/board-article/board-article.resolver.ts
+++ apps/realstate-api/src/components/board-article/board-article.resolver.ts
@@ -2,11 +2,16 @@ import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ObjectId } from 'mongoose';
 import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
-import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import { AllBoardArticlesInquiry, BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
+import { Properties } from '../../libs/dto/property/property';
+import { AllPropertiesInquiry } from '../../libs/dto/property/property.input';
+import { MemberType } from '../../libs/enums/member.enum';
 import { shapeIntoMongoObjectId } from '../../libs/types/config';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { BoardArticleService } from './board-article.service';
 
@@ -54,4 +59,38 @@ export class BoardArticleResolver {
         console.log("Mutation: getBoardArticles");
         return await this.boardArticleService.getBoardArticles(memberId, input);
     }
+
+    //--ADMIN--//
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Query(() => BoardArticles)
+    public async getAllArticlesByAdmin(
+        @Args('input') input: AllBoardArticlesInquiry,
+        @AuthMember('_id') memberId: ObjectId ):Promise<BoardArticles> {
+        console.log("Query: getAllArticlesByAdmin");
+        return await this.boardArticleService.getAllArticlesByAdmin(input);
+    }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => BoardArticle)
+    public async updateBoardArticleByAdmin(
+        @Args("input") input: BoardArticleUpdate,
+        @AuthMember('_id') memberId: ObjectId): Promise<BoardArticle> {
+        console.log("Mutation updateBoardArticleByAdmin executed");
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.boardArticleService.updateBoardArticleByAdmin(input);
+    }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => BoardArticle)
+    public async removeBoardArticleByAdmin(@Args("articleId") input: string):Promise<BoardArticle> {
+        console.log("Mutation removeBoardArticleByAdmin executed");
+        const articleId = shapeIntoMongoObjectId(input);
+        return await this.boardArticleService.removeBoardArticleByAdmin(articleId);
+    }
+
+
 }
--- apps/realstate-api/src/components/board-article/board-article.service.ts
+++ apps/realstate-api/src/components/board-article/board-article.service.ts
@@ -3,7 +3,8 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
-import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import { AllBoardArticlesInquiry, BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { BoardArticleStatus } from '../../libs/enums/board-article.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
@@ -108,6 +109,70 @@ export class BoardArticleService {
         return result[0];
     }
 
+
+    //--ADMIN--//
+
+    public async getAllArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
+
+        const {articleStatus, articleCategory} = input.search;
+        const match: T = {};
+        const sort: T = {[input?.sort ?? 'createdAt'] : input?.direction ?? Direction.DESC};
+
+        if(articleStatus) match.articleStatus = articleStatus;
+        if(articleCategory) match.articleCategory = articleCategory;
+
+        console.log("match: =>", match);
+
+        const result = await this.boardArticleSchema
+        .aggregate([
+            {$match: match},
+            {$sort: sort},
+            {
+                $facet: {
+                    list: [
+                        {$skip: (input.page -1) * input.limit},
+                        {$limit: input.limit},
+                        //meLiked
+                        lookupMember,
+                        {$unwind: '$memberData'},
+                    ],
+                    metaCounter: [{$count: 'total'}],
+                }
+            }
+        ]).exec();
+
+        if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        
+        return result[0];
+    }
+
+    public async updateBoardArticleByAdmin(input: BoardArticleUpdate): Promise<BoardArticle> {
+        let {_id, articleStatus} = input;
+
+        const result = await this.boardArticleSchema
+        .findOneAndUpdate( {_id: _id, articleStatus: BoardArticleStatus.ACTIVE}, input, {new: true}).exec();
+        if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+        if(articleStatus === BoardArticleStatus.DELETE) {
+            await this.memberService.memberStatsEditor({
+                _id: result.memberId,
+                targetKey: 'memberArticles',
+                modifier: -1,
+            })
+        }
+        return result;
+    }
+
+    public async removeBoardArticleByAdmin(articleId: ObjectId):Promise<BoardArticle> {
+        const search: T = {_id: articleId, articleStatus: BoardArticleStatus.DELETE};
+        const result = await this.boardArticleSchema.findOneAndDelete(search).exec();
+        if(!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+    
+        return result;
+    }
+
+
+
     private async boardArticleStatsEditor(input: StatisticModifier):Promise<BoardArticle> {
         const {_id, targetKey, modifier} = input;
         console.log("article stats incremented");
@@ -121,4 +186,6 @@ export class BoardArticleService {
         )
         .exec();
     }
+
+  
 }
